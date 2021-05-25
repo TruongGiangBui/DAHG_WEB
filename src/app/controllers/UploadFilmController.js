2e62ae0bc27b2378397c3ba8fcd1225052494d29
TruongGiangBui <truonggiangbui9@gmail.com>
2021-05-26 00:33:24 +0700

    final commit

--- src/app/controllers/UploadFilmController.js
+++ src/app/controllers/UploadFilmController.js
@@ -3,9 +3,16 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class UploadFilmController {
   //[GET] /
-    index(req, res, next) {        
-        console.log("call")
-        res.render("user/uploadfilm")
+  index(req, res, next) { 
+    var err = {
+      1: "Vui lòng điền đầy đủ thông tin",
+      2: "Vui lòng điền đúng định dạng liên kết đến trailer",
+      3: "Vui lòng điền đúng định dạng liên kết đến phim",
+      4: "Phim đã tồn tại"
+        }
+        if(req.query.err)
+          res.render("user/uploadfilm", { alert: err[req.query.err] })
+      else res.render("user/uploadfilm")
     }
 }
 module.exports = new UploadFilmController();
